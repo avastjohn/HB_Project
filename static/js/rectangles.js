0189f031ea7a1102f8902f8ab1c6c3cae32eb5b4
Ava St. John <avasaintjohn@gmail.com>
2013-11-06 10:39:50 -0800

    Added forground map

--- static/js/rectangles.js
+++ static/js/rectangles.js
@@ -5,11 +5,14 @@ window.onload = function() {
 
     var UNIT_SIZE = 100;
 
-    var sampleMap = "GGGp GGpp GppG ppGG"
+    var backMap = "GGGp GGpp GppG ppGG"
+    var foreMap = "nnnt nnnn nnnn annn"
 
     var tiles = {
         "G": "#197c57",
-        "p": "#96b124"
+        "p": "#96b124",
+        "a": "http://i278.photobucket.com/albums/kk90/thrila_from_manila/tiny%20graphics/bth_56916f0f.gif",
+        "t": "http://a0.twimg.com/profile_images/2187028467/tiny_star_normal.png"
     }
 
     var mkSquare = function(x, y, color) {
@@ -22,6 +25,14 @@ window.onload = function() {
         context.stroke();
     }
 
+    var placeGraphic = function(x, y, imgsrc) {
+        var graphic = new Image();
+        graphic.onload = function() {
+            context.drawImage(graphic, (x*UNIT_SIZE+30), (y*UNIT_SIZE+30));
+        };
+        graphic.src = imgsrc;
+    }
+
     var parseMap = function(string) {
         var rows = string.split(" ");
         var parsedRows = [];
@@ -32,7 +43,7 @@ window.onload = function() {
         return parsedRows;
     }
 
-    var drawMap = function(mapString) {
+    var drawBack = function(mapString) {
         var parsedMap = parseMap(mapString);
         for (var i=0; i < parsedMap.length; i++) {
             for (var j=0; j < parsedMap[i].length; j++) {
@@ -42,6 +53,18 @@ window.onload = function() {
         }
     }
 
-    var tester = drawMap(sampleMap);
+    var drawFront = function(mapString) {
+        var parsedMap = parseMap(mapString);
+        for (var i=0; i < parsedMap.length; i++) {
+            for (var j=0; j < parsedMap[i].length; j++) {
+                if (parsedMap[i][j] == "a" || parsedMap[i][j] == "t") {
+                    placeGraphic(j, i, tiles[parsedMap[i][j]]);
+                }
+            }
+        }
+    }
+
+    var backTester = drawBack(backMap);
+    var foreTester = drawFront(foreMap);
 
 }
