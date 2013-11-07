23dab734a229fdcd749fc6788480e6f9ac46f1b5
Ava St. John <avasaintjohn@gmail.com>
2013-11-07 12:00:16 -0800

    Created pet class with draw method

--- static/js/rectangles.js
+++ static/js/rectangles.js
@@ -1,26 +1,26 @@
 // length and width in pixels of the squares on the map
 var UNIT_SIZE = 100;
 
+var myCanvas = document.getElementById('myCanvas');
+var context = myCanvas.getContext("2d");
+
 
 // this data will eventually come from the database:
                     // level one
                 var backMap1 = "GGG ppp GGG";
-                var petStart1 = (0,1);
-                var treatPos1 = (2,1);
+                var petStart1 = [0,1];
+                var treatPos1 = [2,1];
                     // level two
                 var backMap2 = "GGGG GppG ppGG GGGG";
-                var petStart2 = (0,2);
-                var treatPos2 = (2,1);
+                var petStart2 = [0,2];
+                var treatPos2 = [2,1];
                     // level three
                 var backMap3 = "GGGp GGpp GppG ppGG";
-                var petStart3 = (0,3);
-                var treatPos3 = (3,0);
+                var petStart3 = [0,3];
+                var treatPos3 = [3,0];
 
 var gameBoard = function(backMap) {
     this.backMap = backMap;
-
-    this.myCanvas = document.getElementById('myCanvas');
-    this.context = myCanvas.getContext("2d");
 
     this.tiles = {
         "G": "#197c57",
@@ -40,13 +40,13 @@ var gameBoard = function(backMap) {
 
     this.mkSquare = function(x, y, color) {
         // draws a square at position specified and of specified color
-        this.context.beginPath();
-        this.context.rect((x*UNIT_SIZE+30), (y*UNIT_SIZE+30), UNIT_SIZE, UNIT_SIZE);
-        this.context.fillStyle = color;
-        this.context.fill();
-        this.context.lineWidth = 2;
-        this.context.strokeStyle = "#d35577";
-        this.context.stroke();
+        context.beginPath();
+        context.rect((x*UNIT_SIZE+30), (y*UNIT_SIZE+30), UNIT_SIZE, UNIT_SIZE);
+        context.fillStyle = color;
+        context.fill();
+        context.lineWidth = 2;
+        context.strokeStyle = "#d35577";
+        context.stroke();
     };
 
     this.drawBoard = function() {
@@ -68,29 +68,47 @@ var pet = function(pettype, petname, gender) {
     this.petname = petname;
     this.gender = gender;
 
-    var treats = {
+    this.treats = {
         "dog": "bone",
         "bunny": "carrot"
     };
 
-    var images = {
+    this.images = {
         "bunny": "http://i.imgur.com/3i4vM4S.png",
         "carrot": "http://i.imgur.com/F1n6SZW.png",
         "dog": "http://i.imgur.com/sPvA7yE.png",
         "bone": "http://i.imgur.com/nczpv7L.png"
     }
 
-    var move = function(direction) {
+    this.image = this.images[this.pettype];
+    this.treat = this.treats[this.pettype];
+    this.treatImage = this.images[this.treat];
+
+    this.drawPet = function(pos) {
+        //draw pet at position (later will call function by passing in petStart)
+        var petImage = new Image();
+            petImage.onload = function() {
+                context.drawImage(petImage, (pos[0]*UNIT_SIZE+30), (pos[1]*UNIT_SIZE));
+            };
+        petImage.src = this.image;
+    };
+
+    this.drawTreat = function(pos) {
+        // draw treat at position (later will call function by passing in treatPos)
+        return;
+    };
+
+    this.move = function(direction) {
         // change coordinates? or delete and redraw...
         return;
     };
 
-    var sleep = function(delay) {
+    this.sleep = function(delay) {
         // time delay keep bunny in same place
         return;
     };
 
-    var eatTreat = function() {
+    this.eatTreat = function() {
         // make this function later. it will make stars 
         // appear or something when bunny reaches carrot.
         return;
@@ -103,42 +121,7 @@ window.onload = function() {
     var currentBoard = new gameBoard(backMap3);
     currentBoard.drawBoard();
 
+    var mrSnuffles = new pet("bunny", "Mr. Suffles", "m");
+    mrSnuffles.drawPet(petStart3);
     
-
-    
-
-    // var placeGraphic = function(x, y, imgsrc, tall) {
-    //     var graphic = new Image();
-    //     if (tall == true) {
-    //         graphic.onload = function() {
-    //             context.drawImage(graphic, (x*UNIT_SIZE+30), (y*UNIT_SIZE));
-    //         };
-    //     } else {
-    //         graphic.onload = function() {
-    //             context.drawImage(graphic, (x*UNIT_SIZE+30), (y*UNIT_SIZE+30));
-    //         };
-    //     }
-    //     graphic.src = imgsrc;
-    // }
-
-
-
-
-
-    // var drawFront = function(mapString) {
-    //     var parsedMap = parseMap(mapString);
-    //     for (var i=0; i < parsedMap.length; i++) {
-    //         for (var j=0; j < parsedMap[i].length; j++) {
-    //             if (parsedMap[i][j] == "a") {
-    //                 placeGraphic(j, i, tiles[parsedMap[i][j]], true);
-    //             } else if (parsedMap[i][j] == "t") {
-    //                 placeGraphic(j, i, tiles[parsedMap[i][j]], false);
-    //             }
-    //         }
-    //     }
-    // }
-
-    // var backTester = drawBack(backMap);
-    // var foreTester = drawFront(foreMap);
-
 };
